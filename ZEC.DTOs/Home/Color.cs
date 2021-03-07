e8bc0b284bbd354a5f85436825436f02c9cc350c
HMZ <hamzi1995@gmail.com>
2021-03-07 15:46:25 -0500

    Finish promo block component.

---
+++ ZEC.DTOs/Home/Color.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZEC.DTOs.Home
+{
+    public enum Color
+    {
+        Black = 1,
+        Cyan = 2,
+        Magenta = 3,
+        Orange = 4,
+        Red = 5,
+        Brown = 6,
+        Maroon = 7,
+        Blue = 8,
+        Green = 9
+    }
+}
---
+++ ZEC.DTOs/Home/PromoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZEC.DTOs.Home
+{
+    public class PromoModel
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string ButtonText { get; set; }
+        public Color BackgroundColor { get; set; }
+    }
+}
