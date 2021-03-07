998d9117f3da8ce92d9b2422cf44c19c3dbcc6b1
HMZ <hamzi1995@gmail.com>
2021-03-07 14:38:38 -0500

    Finish the carousel component.

--- ZEC.Pages/wwwroot/js/home.js
+++ ZEC.Pages/wwwroot/js/home.js
@@ -1,7 +1,6 @@
 ﻿//Swiper Config
 //================================================
 
-import Swiper from 'swiper/bundle';
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     loop: true,
@@ -9,18 +8,8 @@ const swiper = new Swiper('.swiper-container', {
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+        clickable: true
+    }
 });
 
 //================================================
