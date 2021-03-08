6bfcf01428777ae5d9ed0cb486a3db267f58a6f8
HMZ <hamzi1995@gmail.com>
2021-03-07 20:44:08 -0500

    Restructure carousel logic.

---
+++ ZEC.Core/Models/Carousel/CarouselModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZEC.Core.Models.Helpers;
+
+namespace ZEC.Core.Models.Carousel
+{
+    public class CarouselModel
+    {
+        public CarouselModel()
+        {
+            Position = Position.Center;
+        }
+
+        public string HeaderText { get; set; }
+        public string BodyText { get; set; }
+        public string PictureUrl { get; set; }
+        public string Link { get; set; }
+        public string ButtonText { get; set; }
+        public Position Position { get; set; }
+        public bool IsExternalUrl { get; set; }
+        [NotMapped]
+        public bool HasLink { get { return !string.IsNullOrEmpty(Link) || !string.IsNullOrEmpty(ButtonText); } }
+    }
+}
---
+++ ZEC.Core/Models/Helpers/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZEC.Core.Models.Helpers
+{
+    public enum Position : int
+    {
+        Left = 1,
+        Right = 2,
+        Center = 3
+    }
+}
