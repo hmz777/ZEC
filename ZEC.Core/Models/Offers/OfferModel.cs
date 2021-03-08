6449a0e54d743582b1d08706db4bdab04e2b4f0b
HMZ <hamzi1995@gmail.com>
2021-03-08 14:46:06 -0500

    Add offer block component and its dummy data.

---
+++ ZEC.Core/Models/Offers/OfferModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZEC.Core.Models.Offers
+{
+    public class OfferModel
+    {
+        public string TitleText { get; set; }
+        public string BodyText { get; set; }
+        public string PictureUrl { get; set; }
+        public string AltText { get; set; }
+        public string Link { get; set; }
+        public bool HasLink { get { return !string.IsNullOrEmpty(Link); } }
+        public bool IsExternalLink { get; set; }
+    }
+}
