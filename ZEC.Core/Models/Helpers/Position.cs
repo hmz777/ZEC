d38f6ee3162bddbc9b721c04f18e7e45ebaaeb08
HMZ <hamzi1995@gmail.com>
2021-03-08 20:45:36 -0500

    Add more general choices for position.

--- ZEC.Core/Models/Helpers/Position.cs
+++ ZEC.Core/Models/Helpers/Position.cs
@@ -10,6 +10,10 @@ namespace ZEC.Core.Models.Helpers
     {
         Left = 1,
         Right = 2,
-        Center = 3
+        Center = 3,
+        UpperRight = 4,
+        BottomRight = 5,
+        UpperLeft = 6,
+        BottomLeft = 7
     }
 }
