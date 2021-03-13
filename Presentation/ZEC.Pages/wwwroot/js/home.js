33faf210d98dd23b12849ba2e6b4e4203759dea4
HMZ <hamzi1995@gmail.com>
2021-03-13 17:19:45 -0500

    Add bootstrap-star-rating library.

--- Presentation/ZEC.Pages/wwwroot/js/home.js
+++ Presentation/ZEC.Pages/wwwroot/js/home.js
@@ -1,35 +1,41 @@
-﻿//Swiper Config (Main carousel)
-//================================================
-const swiper1 = new Swiper('.swiper1', {
-    loop: true,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    }
+﻿$(function () {
+    //Swiper Config (Main carousel)
+    //================================================
+    const swiper1 = new Swiper('.swiper1', {
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        }
+    });
+    //================================================
+    //Swiper Config (Popular categories carousel)
+    //================================================
+    const swiper2 = new Swiper('.swiper2', {
+        slidesPerView: '6',
+        spaceBetween: 25,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+    //================================================
+    //Swiper Config (Sales zone)
+    //================================================
+    const swiper3 = new Swiper('.swiper3', {
+        slidesPerView: '5',
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+    //================================================
+    //Review stars config
+    //================================================
+    $(".product-review-stars").rating({ "displayOnly": true, "showCaption": false });
+    //================================================
 });
-//================================================
-//Swiper Config (Popular categories carousel)
-//================================================
-const swiper2 = new Swiper('.swiper2', {
-    slidesPerView: '6',
-    spaceBetween: 25,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
-});
-//================================================
-//Swiper Config (Sales zone)
-//================================================
-const swiper3 = new Swiper('.swiper3', {
-    slidesPerView: '5',
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
-});
-//================================================
