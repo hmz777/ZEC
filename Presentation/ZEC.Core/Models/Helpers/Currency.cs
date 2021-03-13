ec82487a6815c1ad4d6be3e8e7bcc291fbf6bb6a
HMZ <hamzi1995@gmail.com>
2021-03-13 17:22:37 -0500

    Update product model and add currency enum.

---
+++ Presentation/ZEC.Core/Models/Helpers/Currency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZEC.Core.Models.Helpers
+{
+    public enum Currency : int
+    {
+        USDollar = 1,
+        Euro = 2,
+        Pound = 3,
+        SyrianPound = 4
+    }
+}
--- Presentation/ZEC.Core/Models/Product/ProductModel.cs
+++ Presentation/ZEC.Core/Models/Product/ProductModel.cs
@@ -1,24 +1,32 @@
 ﻿using System.Collections.Generic;
+using ZEC.Core.Models.Helpers;
 
 namespace ZEC.Core.Models.Product
 {
     public class ProductModel
     {
+        public ProductModel()
+        {
+            Currency = Currency.USDollar;
+        }
+
         public string Name { get; set; }
         public string SKU { get; set; }
-        public ICollection<string> Pictures { get; set; }
+        public List<string> Pictures { get; set; }
         public string Manufacturer { get; set; }
-        public bool IsOnSale { get; set; }
-        public string SaleValue { get; set; }
+        public bool HasDiscount { get; set; }
+        public Currency Currency { get; set; }
+        public decimal SaleValue { get { return OldPrice - Price; } }
         public decimal OldPrice { get; set; }
-        public decimal NewPrice { get; set; }
+        public decimal Price { get; set; }
         public int No_Reviews { get; set; }
-        public int OverallRating { get; set; }
+        public double OverallRating { get; set; }
         public bool IsInStock { get; set; }
         public int InventoryUnits { get; set; }
         public bool IsStoreSelection { get; set; }
         public bool HasColors { get; set; }
-        public ICollection<ProductColor> ProductColors { get; set; }
+        public List<ProductColor> ProductColors { get; set; }
         public int RequestedQuantity { get; set; }
+        public bool HasTierPrices { get; set; }
     }
 }
