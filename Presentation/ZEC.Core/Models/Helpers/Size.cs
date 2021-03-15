6d63c98a8ada104e744ab13034d29cd4c8786012
HMZ <hamzi1995@gmail.com>
2021-03-14 23:47:04 -0400

    Add size property for general UI use.

---
+++ Presentation/ZEC.Core/Models/Helpers/Size.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZEC.Core.Models.Helpers
+{
+    public enum Size : int
+    {
+        ExtraSmall = 1,
+        Small = 2,
+        Medium = 3,
+        Large = 4,
+        ExtraLarge = 5
+    }
+}
--- Presentation/ZEC.Core/Models/Promotions/PromoModel.cs
+++ Presentation/ZEC.Core/Models/Promotions/PromoModel.cs
@@ -8,6 +8,7 @@ namespace ZEC.Core.Models.Promotions
         public string Text { get; set; }
         public string ButtonText { get; set; }
         public Color BackgroundColor { get; set; }
+        public Size Size { get; set; }
         public string PictureUrl { get; set; }
         public bool IsExternalLink { get; set; }
         public string Link { get; set; }
