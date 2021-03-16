94c1925e362a3f8fbb1abd6e46be54c0fbf2c66f
HMZ <hamzi1995@gmail.com>
2021-03-16 17:46:47 -0400

    Make the background color a string type in order to freely select the background color.

--- Presentation/ZEC.Core/Models/Promotions/PromoModel.cs
+++ Presentation/ZEC.Core/Models/Promotions/PromoModel.cs
@@ -7,7 +7,8 @@ namespace ZEC.Core.Models.Promotions
         public string Title { get; set; }
         public string Text { get; set; }
         public string ButtonText { get; set; }
-        public Color BackgroundColor { get; set; }
+        public string BackgroundColor { get; set; }
+        public bool HasDarkBackground { get; set; }
         public Size Size { get; set; }
         public string PictureUrl { get; set; }
         public bool IsExternalLink { get; set; }
