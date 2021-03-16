b5bf144d4e7da5ea0f393edd3eeae00d5888e4b3
HMZ <hamzi1995@gmail.com>
2021-03-16 17:46:12 -0400

    Add the about section and its dummy data.

---
+++ Presentation/ZEC.Core/Models/About/AboutModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZEC.Core.Models.About
+{
+    public class AboutModel
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
